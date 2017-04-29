c1c4c688bb737628cd7f38e9c0d7f71a6343ec4c
Jim Martens <github@2martens.de>
2017-04-29 23:31:13 +0200

    Reformatted code and used strict mode for JS where that wasn't used before
    Signed-off-by: Jim Martens <github@2martens.de>

--- faq.php
+++ faq.php
@@ -1,103 +1,115 @@
-<?php 
-	isset($_GET['from']) ? $back = $_GET['from'] : $back = "index.php";
-	if($back != 'index.php' and substr($back, 0, 14) != 'multiplier.php' and substr($back, 0, 10) != 'result.php' and substr($back, 0, 12) != 'mahlowat.php'){
-		$back = "index.php";
-	}
-	
-	$answerstring = '';
-	if(isset($_POST['ans'])){
-		$answerstring = $_POST['ans'];
-	}
-	
-	$count = 'undefined';
-	if(isset($_POST['count'])){
-		$count = $_POST['count'];
-	}
-    
+<?php
+isset($_GET['from']) ? $back = $_GET['from'] : $back = "index.php";
+if ($back != 'index.php' and substr($back, 0, 14) != 'multiplier.php' and substr($back, 0, 10) != 'result.php' and substr($back, 0, 12) != 'mahlowat.php') {
+    $back = "index.php";
+}
+
+$answerstring = '';
+if (isset($_POST['ans'])) {
+    $answerstring = $_POST['ans'];
+}
+
+$count = 'undefined';
+if (isset($_POST['count'])) {
+    $count = $_POST['count'];
+}
+
 ?>
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <title>Green-o-mat - FAQ</title>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
     <meta content="">
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-    
+
     <link rel="stylesheet" type="text/css" href="css/style.css">
-  </head>
-  <body>
-  
-  <script src="js/jquery-2.0.2.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/mahlowat.js"></script>
-  
-  <div class="container" style="margin-top: 20px;">
-      <img src="img/gruene-logo.jpg" title="GRÜNEN Logo" class="pull-right" onclick="changeText()" style="height: 125px" />
-	<p id="spruch" class="pull-right"></p>
-      <div class="bottom-buffer top-buffer">
-    
-    <h1>FAQ</h1>
+</head>
+<body>
 
-    <h4>Wer macht den Green-o-mat?</h4>
-    <p>Motivierte Menschen der GRÜNEN.</p>
-    
-    <h4>Wer hat die Thesen erarbeitet?</h4>
-    <p>Eine Arbeitsgruppe auf dem 5. Camp Netzbegrünung der GRÜNEN.</p>
-    
-    <h4>Von welcher Wahl reden wir hier überhaupt?</h4>
-    <p>Bundestagswahl 2017</p>
-    
-    <h4>Wer hat das hier programmiert?</h4>
-    <p>Der <a href="http://jim.2martens.de" title="jim.2martens.de" target="_blank">Jim</a>, weil der das kann.</p>
-    
-    <h4>Funktioniert das hier wie der "echte" Wahl-O-Mat der bpb?</h4>
-    <p>Nein.</p>
-    
-    <h4>Wie werden die Punkte berechnet?</h4>
-    <p>Die Antworten der Testperson (das bist du) werden mit den vorgegebenen Antworten der GRÜNEN abgeglichen.</p>
-    <ul>
-      <li>Stimmt die Antwort überein, werden den GRÜNEN 2 Punkte gutgeschrieben;</li>
-      <li>Weicht die Antwort leicht ab (Zustimmung/Neutral oder Neutral/Ablehnung), wird den GRÜNEN 1 Punkt gutgeschrieben;</li>
-      <li>Sind die Antworten entgegengesetzt, gibt es keine Punkte für die Gruppe.</li>
-    </ul>
-    <p>Eine Frage, die die Testperson übersprungen hat, wird nicht gewertet. Entsprechend gibt es dann insgesamt weniger Punkte zu erreichen.</p>
-    <p>Eine Frage, die doppelt gewichtet werden soll, wird doppelt gewichtet, das heißt, für sie wird die doppelte Punktzahl gutgeschrieben (0/2/4). Entsprechend gibt es insgesamt mehr Punkte zu erreichen.</p>
-    
-    <div id='log'>
-    <h4>Werden meine Antworten gespeichert?</h4>
-    <p>Vor der Auswertung wirst du gefragt, ob wir deine Antworten speichern dürfen.<br>
-    Du kannst dann "Ja" auswählen, was bedeutet, dass wir deine Antwortkombination zusammen mit einer ID speichern, die aus deiner IP-Adresse und einem täglich wechselnden zufälligen Wert errechnet wird. Bereits am nächsten Tag kann dein Eintrag auf keinen Fall mehr einer konkreten IP-Adresse zugeordnet werden.<br>
-    Wenn du "Nein" auswählst, wird lediglich ein Zähler um 1 erhöht.</p>
-    <p></p>
-    
-    <h4>Ich möchte gar nicht gezählt werden!</h4>
-    <p>Unser Webserver legt bei jedem Seitenaufruf einen Log-Eintrag an, der unter anderem einen Zeitstempel, deine IP-Adresse und die aufgerufene URL enthält. Es wäre also unaufrichtig, dir vorzumachen, dass dein Aufruf des Mahlowat nicht gezählt wird. Der Mahlowat wurde jedoch so konzipiert, dass aus den Server-Logdateien nicht ersichtlich ist, welche Antworten du ausgewählt hast. Dies sehen wir tatsächlich nur, wenn du dem am Ende explizit zustimmst.</p>
+<script src="js/jquery-2.0.2.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/mahlowat.js"></script>
+
+<div class="container" style="margin-top: 20px;">
+    <img src="img/gruene-logo.jpg" title="GRÜNEN Logo" class="pull-right" onclick="changeText()" style="height: 125px"/>
+    <p id="spruch" class="pull-right"></p>
+    <div class="bottom-buffer top-buffer">
+
+        <h1>FAQ</h1>
+
+        <h4>Wer macht den Green-o-mat?</h4>
+        <p>Motivierte Menschen der GRÜNEN.</p>
+
+        <h4>Wer hat die Thesen erarbeitet?</h4>
+        <p>Eine Arbeitsgruppe auf dem 5. Camp Netzbegrünung der GRÜNEN.</p>
+
+        <h4>Von welcher Wahl reden wir hier überhaupt?</h4>
+        <p>Bundestagswahl 2017</p>
+
+        <h4>Wer hat das hier programmiert?</h4>
+        <p>Der <a href="http://jim.2martens.de" title="jim.2martens.de" target="_blank">Jim</a>, weil der das kann.</p>
+
+        <h4>Funktioniert das hier wie der "echte" Wahl-O-Mat der bpb?</h4>
+        <p>Nein.</p>
+
+        <h4>Wie werden die Punkte berechnet?</h4>
+        <p>Die Antworten der Testperson (das bist du) werden mit den vorgegebenen Antworten der GRÜNEN abgeglichen.</p>
+        <ul>
+            <li>Stimmt die Antwort überein, werden den GRÜNEN 2 Punkte gutgeschrieben;</li>
+            <li>Weicht die Antwort leicht ab (Zustimmung/Neutral oder Neutral/Ablehnung), wird den GRÜNEN 1 Punkt
+                gutgeschrieben;
+            </li>
+            <li>Sind die Antworten entgegengesetzt, gibt es keine Punkte für die Gruppe.</li>
+        </ul>
+        <p>Eine Frage, die die Testperson übersprungen hat, wird nicht gewertet. Entsprechend gibt es dann insgesamt
+            weniger Punkte zu erreichen.</p>
+        <p>Eine Frage, die doppelt gewichtet werden soll, wird doppelt gewichtet, das heißt, für sie wird die doppelte
+            Punktzahl gutgeschrieben (0/2/4). Entsprechend gibt es insgesamt mehr Punkte zu erreichen.</p>
+
+        <div id='log'>
+            <h4>Werden meine Antworten gespeichert?</h4>
+            <p>Vor der Auswertung wirst du gefragt, ob wir deine Antworten speichern dürfen.<br>
+                Du kannst dann "Ja" auswählen, was bedeutet, dass wir deine Antwortkombination zusammen mit einer ID
+                speichern, die aus deiner IP-Adresse und einem täglich wechselnden zufälligen Wert errechnet wird.
+                Bereits am nächsten Tag kann dein Eintrag auf keinen Fall mehr einer konkreten IP-Adresse zugeordnet
+                werden.<br>
+                Wenn du "Nein" auswählst, wird lediglich ein Zähler um 1 erhöht.</p>
+            <p></p>
+
+            <h4>Ich möchte gar nicht gezählt werden!</h4>
+            <p>Unser Webserver legt bei jedem Seitenaufruf einen Log-Eintrag an, der unter anderem einen Zeitstempel,
+                deine IP-Adresse und die aufgerufene URL enthält. Es wäre also unaufrichtig, dir vorzumachen, dass dein
+                Aufruf des Mahlowat nicht gezählt wird. Der Mahlowat wurde jedoch so konzipiert, dass aus den
+                Server-Logdateien nicht ersichtlich ist, welche Antworten du ausgewählt hast. Dies sehen wir tatsächlich
+                nur, wenn du dem am Ende explizit zustimmst.</p>
+        </div>
+
+        <h4>Ich habe einen Fehler gefunden!</h4>
+        <p>Dann solltest du das melden. Wir freuen uns über sachdienliche Hinweise.</p>
+
+        <a class="btn btn-primary" href="<?php echo $back; ?>" onclick="callPage(event, '<?php echo $back; ?>')"
+           title="Zurück zum Mahlowat">Zurück zum Green-o-mat</a>
     </div>
-    
-    <h4>Ich habe einen Fehler gefunden!</h4>
-    <p>Dann solltest du das melden. Wir freuen uns über sachdienliche Hinweise.</p>
-
-    <a class="btn btn-primary" href="<?php echo $back; ?>" onclick="callPage(event, '<?php echo $back; ?>')" title="Zurück zum Mahlowat">Zurück zum Green-o-mat</a>
-  </div>
-  </div>
+</div>
 
 
-  <script type="text/javascript">
-	$( document ).ready( function(){
-		if(window.location.hash == '#log'){
-			setTimeout(function() {
-				$('#log').css("background-color","#ffff55");
-			}, 1000);
-		}
-	} );
-  
-      function callPage(evt, action){
-		evt.preventDefault();
-		var html = "<input name='ans' value='<?php echo $answerstring;?>'/><input name='count' value='<?php echo $count; ?>'/>";
-		$('<form style="display: none;" method="post"/>').attr('action', action).html(html).appendTo('body').submit();
-	}
-  </script>
+<script type="text/javascript">
+    $(document).ready(function () {
+        if (window.location.hash == '#log') {
+            setTimeout(function () {
+                $('#log').css("background-color", "#ffff55");
+            }, 1000);
+        }
+    });
 
-  
-  </body>
+    function callPage(evt, action) {
+        evt.preventDefault();
+        var html = "<input name='ans' value='<?php echo $answerstring;?>'/><input name='count' value='<?php echo $count; ?>'/>";
+        $('<form style="display: none;" method="post"/>').attr('action', action).html(html).appendTo('body').submit();
+    }
+</script>
+
+
+</body>
 </html>
--- index.php
+++ index.php
@@ -4,51 +4,59 @@ $baseurl = "http://" . $_SERVER['SERVER_NAME'] . $uri_parts[0];
 ?>
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <title>Green-o-mat</title>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
     <meta content="Green-o-mat">
-    
+
     <meta name="image_src" content="img/gruene-logo.jpg"/>
-    <meta name="description" content="Mit dem Green-o-mat kann die eigene Position in Verhältnis zu den GRÜNEN getestet werden."/>
-    
+    <meta name="description"
+          content="Mit dem Green-o-mat kann die eigene Position in Verhältnis zu den GRÜNEN getestet werden."/>
+
     <meta property="og:title" content="Mahlowat"/>
-    <meta property="og:type"  content="website"/>
+    <meta property="og:type" content="website"/>
     <meta property="og:image" content="img/gruene-logo.jpg"/>
-    <meta property="og:url"   content=""/>
+    <meta property="og:url" content=""/>
     <meta property="og:site-name" content="example.com"/>
-    <meta property="og:description" content="Mit dem Green-o-mat kann die eigene Position in Verhältnis zu den GRÜNEN getestet werden."/>
-    
+    <meta property="og:description"
+          content="Mit dem Green-o-mat kann die eigene Position in Verhältnis zu den GRÜNEN getestet werden."/>
+
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
     <link rel="stylesheet" href="css/font-awesome.min.css">
-    
+
     <link rel="stylesheet" type="text/css" href="css/style.css">
-    
+
     <link href="shariff/shariff.min.css" rel="stylesheet">
-    
+
     <script src="js/jquery-2.0.2.min.js"></script>
-    
-  </head>
-  <body>
-  
-  <div class="container mow-container" style="margin-top:20px;">
-  
-  <div class="text-center">
-  
-    <img src="img/gruene-logo.jpg" title="GRÜNE Logo" style="height: 125px" />
-    
-    <h1><small>Der</small> Green-o-mat</h1>
-  </div>
+
+</head>
+<body>
+
+<div class="container mow-container" style="margin-top:20px;">
+
+    <div class="text-center">
+
+        <img src="img/gruene-logo.jpg" title="GRÜNE Logo" style="height: 125px"/>
+
+        <h1>
+            <small>Der</small>
+            Green-o-mat
+        </h1>
+    </div>
     <p>Mit dem Green-o-mat könnt Ihr überprüfen, wie gut die GRÜNEN zu Euch oder Ihr zu den GRÜNEN passt.</p>
-    
-    <p class="text-center"><a class="btn btn-large btn-primary" href="mahlowat.php" title="Green-o-mat starten">Mit der Befragung beginnen!</a></p>
-    
-    <p class="text-center"><a href="faq.php" title="Fragen und Antworten"><small>FAQ</small></a></p>
-    
-    
+
+    <p class="text-center"><a class="btn btn-large btn-primary" href="mahlowat.php" title="Green-o-mat starten">Mit der
+            Befragung beginnen!</a></p>
+
+    <p class="text-center"><a href="faq.php" title="Fragen und Antworten">
+            <small>FAQ</small>
+        </a></p>
+
+
     <div class="shariff" data-url="<?php echo $baseurl; ?>" data-referrer-track=null></div>
     <script src="shariff/shariff.min.js"></script>
-  </div>
-  
-  </body>
+</div>
+
+</body>
 </html>
