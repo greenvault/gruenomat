418e4a7b6c78622593a15ff0097fe62ee08ed880
Jim Martens <github@2martens.de>
2017-04-29 20:42:52 +0200

    Adapted the start page to Green-o-mat
    Signed-off-by: Jim Martens <github@2martens.de>

--- index.php
+++ index.php
@@ -5,19 +5,19 @@ $baseurl = "http://" . $_SERVER['SERVER_NAME'] . $uri_parts[0];
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Mahlowat</title>
+    <title>Green-o-mat</title>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-    <meta content="Mahlowat">
+    <meta content="Green-o-mat">
     
-    <meta name="image_src" content="img/mahlowat_logo.png"/>
-    <meta name="description" content="Der Mahlowat ist ein Angebot von XYZ und wurde auf Beschluss des XXXV. Studierendenparlaments der Uni Bonn entwickelt. Er ermöglicht es, zu ausgewählten Themen die eigenen Standpunkte mit denen der Listen abzugleichen, die zur Wahl antreten."/>
+    <meta name="image_src" content="img/gruene-logo.jpg"/>
+    <meta name="description" content="Mit dem Green-o-mat kann die eigene Position in Verhältnis zu den GRÜNEN getestet werden."/>
     
     <meta property="og:title" content="Mahlowat"/>
     <meta property="og:type"  content="website"/>
-    <meta property="og:image" content="img/mahlowat_logo.png"/>
+    <meta property="og:image" content="img/gruene-logo.jpg"/>
     <meta property="og:url"   content=""/>
     <meta property="og:site-name" content="example.com"/>
-    <meta property="og:description" content="Der Mahlowat ist ein Angebot von XYZ und wurde auf Beschluss des XXXV. Studierendenparlaments der Uni Bonn entwickelt. Er ermöglicht es, zu ausgewählten Themen die eigenen Standpunkte mit denen der Listen abzugleichen, die zur Wahl antreten."/>
+    <meta property="og:description" content="Mit dem Green-o-mat kann die eigene Position in Verhältnis zu den GRÜNEN getestet werden."/>
     
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
     <link rel="stylesheet" href="css/font-awesome.min.css">
@@ -35,22 +35,15 @@ $baseurl = "http://" . $_SERVER['SERVER_NAME'] . $uri_parts[0];
   
   <div class="text-center">
   
-    <img src="img/mahlowat_logo.png" title="Mahlowat Logo"/>
+    <img src="img/gruene-logo.jpg" title="GRÜNE Logo" />
     
-    <h1><small>Der</small> Mahlowat</h1>
+    <h1><small>Der</small> Green-o-mat</h1>
   </div>
-    <p>Der Mahlowat ist ein technisches Hilfsmittel, das es ermöglicht, zu ausgewählten Themen die eigenen Standpunkte mit denen der Listen abzugleichen, die zur $Wahl antreten.</p>
-    
-    <p>Er ist selbstverständlich nur als Automat ohne Hirn zu verstehen und spricht keine Wahlempfehlungen aus.</p>
-    
-    <p>Lorem Ipsum.</p>
-    
-    <p>Für ihre Stellungnahmen zu den Thesen sind die Listen selbst verantwortlich.</p>
+    <p>Mit dem Green-o-mat könnt Ihr überprüfen, wie gut die GRÜNEN zu Euch oder Ihr zu den GRÜNEN passt.</p>
     
     <p class="text-center"><a class="btn btn-large btn-primary" href="mahlowat.php" title="Mahlowat starten">Mit der Befragung beginnen!</a></p>
     
     <p class="text-center"><a href="faq.php" title="Fragen und Antworten"><small>FAQ</small></a></p>
-    
     
     
     <div class="shariff" data-url="<?php echo $baseurl; ?>" data-referrer-track=null></div>
